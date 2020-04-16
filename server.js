5da117326bbde0e0731f49837e9f7153227d736c
Natanael <natanael_magalhaes@msn.com>
2020-04-15 22:04:04 -0300

    download endpoint

--- server.js
+++ server.js
@@ -1,8 +1,30 @@
 const express = require('express')
+const ytdl = require('ytdl-core')
+const ffmpeg = require('fluent-ffmpeg')
 const app = express()
 
 app.get('/', (req, res) => {
   res.send(process.env)
+})
+
+app.get('/download/:id', (req, res) => {
+  const url = `https://youtube.com/watch?v=${req.params.id}`
+  const stream = ytdl(url)
+  const proc = new ffmpeg({ source: stream })
+  proc.setFfmpegPath('ffmpeg')
+  proc.withAudioBitrate(128)
+  proc.toFormat('mp3')
+  res.writeHead(200, {
+    'Content-Type': 'audio/mp3',
+    'Content-Disposition': 'attachment'
+  })
+  proc.on('error', (err, stdout, stderr) => {
+    console.log(`Error: ${err.message}`)
+  })
+  proc.on('end', (stdout, stderr) => {
+    console.log('Transcoding succeeded!')
+  })
+  proc.pipe(res, { end: true })
 })
 
 app.listen(process.env.PORT || 3000, () => {
