05804aaa017bbda4895539577bceb9c1ead4f943
Natanael <natanael_magalhaes@msn.com>
2020-04-15 21:04:56 -0300

    start app

---
+++ server.js
@@ -0,0 +1,10 @@
+const express = require('express')
+const app = express()
+
+app.get('/', (req, res) => {
+  res.send('All Ok!')
+})
+
+app.listen(process.env.PORT || 3000, () => {
+  console.log(`App runing at port ${process.env.PORT || 3000}`)
+})
